605dd4f12950db963fa5ff36db028df4422dd9c7
ap <a.spatel1486@gmail.com>
2026-01-10 22:52:17 +0530

    "sms parsing related backend changes"

--- finance-tracker-backend/src/main/java/com/finance/tracker/transactions/domain/BankTemplate.java
+++ finance-tracker-backend/src/main/java/com/finance/tracker/transactions/domain/BankTemplate.java
@@ -1,17 +1,29 @@
 package com.finance.tracker.transactions.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.annotation.PostConstruct;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
+import java.util.regex.Pattern;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class BankTemplate {
 
+    private String name;
     private String pattern;
     private List<String> fields;
+
+    @JsonIgnore
+    private Pattern compiledPattern;
+
+    @PostConstruct
+    public void compile() {
+        this.compiledPattern = Pattern.compile(pattern, Pattern.CASE_INSENSITIVE);
+    }
 
 }
