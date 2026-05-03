daa4dc08800b6c93175b0288d491eecdb93e8a07
ap <a.spatel1486@gmail.com>
2026-05-03 21:25:04 +0530

    "authentication and JWT Implementation"

---
+++ finance-tracker-backend/src/main/java/com/finance/tracker/auth/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.finance.tracker.auth.repository;
+
+import com.finance.tracker.auth.domain.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+    Optional<User> findByEmail(String email);
+}
